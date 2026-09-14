894e79e59fec6a16918edc9ee0c431c3b348fe10
islomovelyor85-spec <islomovelyor85@gmail.com>
2026-09-14 21:03:24 +0000

    Fix syntax error in AuthGate

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,8 @@ import StudentsTracking from './pages/admin/StudentsTracking'
 import SubmissionsReview from './pages/admin/SubmissionsReview'
 import PaymentsReview from './pages/admin/PaymentsReview'
 
-function AuthGate({ children }) 
+function AuthGate({ children }) {
+ 
   // Telegram Mini App ichida bo'lsa ham, oddiy brauzerda bo'lsa ham,
   // dastlabki sessiya tekshiruvi tugaguncha kutamiz.
   const { loading } = useAuth()
